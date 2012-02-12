131066e502bcfae847922cc6d2f7426a94100815
Josh Ellington <hello@joshellington.com>
2012-02-11 18:11:15 -0800

    tweaked styles, added color bars

--- js/script.js
+++ js/script.js
@@ -4,7 +4,28 @@
 
 
 
+$(function() {
+  build();
+});
 
 
 
+function build() {
+  var squares = $('#squares'),
+      windowHeight = $(document).height(),
+      total = 10,
+      i = 0;
 
+  // log(total);
+  squares.empty();
+
+  for(i;i<=total;i++) {
+    // log(i);
+    var square = $('<div class="square"/>').css('background-color', Colors.rand());
+    squares.append(square);
+
+    if ( i == total ) {
+      setTimeout('build()', 5000);
+    }
+  }
+}
